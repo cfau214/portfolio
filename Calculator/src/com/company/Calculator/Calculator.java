ba700c2bcdcf5d5c923ee52e433f1ebdbd3c4294
= <=>
2018-10-17 15:54:47 -0400

    Created Calculator Class
    Basic functions Add, Multiply, Divide. Also created overloaded functions
    for parameters that use double. Created a multiply function that uses
    recursion.

---
+++ Calculator/src/com/company/Calculator/Calculator.java
@@ -0,0 +1,84 @@
+package com.company.Calculator;
+
+/**
+ * This class creates a calculator object that has all of the non-scientific functions of a calculator.
+ *
+ */
+
+public class Calculator {
+
+    /**
+     * Default Constructor.
+     */
+    public Calculator() {}
+
+    /**
+     * Adds two integers n,m and returns the result.
+     * @param n int - First number.
+     * @param m int - Second number.
+     * @return int - Result of n + m.
+     */
+    public int add(int n, int m)
+    {
+        return n+m;
+    }
+
+    /**
+     * Adds two decimal numbers n,m and returns the result.
+     * @param n double - First number.
+     * @param m double - Second number.
+     * @return double - Result of n + m.
+     */
+    public double add(double n, double m)
+    {
+        return n+m;
+    }
+
+    /**
+     * Multiplies two integers n,m and returns the result.
+     * @param n int - First number.
+     * @param m int - Second number.
+     * @return int - Result of n * m.
+     */
+    public int mult(int n, int m)
+    {
+        return n*m;
+    }
+
+    /**
+     * Multiplies two decimal numbers n,m and returns the result.
+     * @param n double - First number.
+     * @param m double - Second number.
+     * @return double - Result of n * m.
+     */
+    public double mult(double n, double m)
+    {
+        return n*m;
+    }
+
+    /**
+     * Multiplies two integers n,m RECURSIVELY and returns the result.
+     * @param n int - First number.
+     * @param m int - Second number.
+     * @return int - Result of n + n + n + ... 'm' number of times.
+     */
+    public int multRecur(int n, int m)
+    {
+        if (m == 0)
+            return 0;
+        else {
+            return n + multRecur(n, m-1);
+        }
+    }
+
+    /**
+     * Divides two doubles n,m and returns the result.
+     * @param n int - Numerator.
+     * @param d int - Denominator.
+     * @return int - Result of n / d.
+     */
+    public double div (int n, int d)
+    {
+        return (double) n/d;
+    }
+}
