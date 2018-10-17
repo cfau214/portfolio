c65000471930970334552f9a5093364fc982ea1d
= <=>
2018-10-17 16:13:43 -0400

    Added JUnit Integration.

---
+++ Calculator/src/com/company/Calculator/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.company.Calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplyRecursive() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void div() {
+    }
+}
