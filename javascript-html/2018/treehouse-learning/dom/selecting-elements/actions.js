59fb1e186fb68f2b456b847937dbb8f760be1e8e
cfau214 <c.fauteux@my.ccsu.edu>
2019-01-09 17:25:43 -0500

    Renamed folder trees.
    Added index.html and actions.js to javascript/treehouse learning.

---
+++ javascript-html/2018/treehouse-learning/dom/selecting-elements/actions.js
@@ -0,0 +1,10 @@
+var change = function() {
+
+    el = document.getElementById('btn1');
+    
+    if (el.value == 'My Button') 
+        el.value = 'button clicked';
+    else 
+        el.value = 'My Button';
+    
+}
