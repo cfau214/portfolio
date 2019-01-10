4d180c34e74ac90bc652c353da1571ae70aac20b
cfau214 <c.fauteux@my.ccsu.edu>
2019-01-09 22:20:16 -0500

    Refactored folder tree.

---
+++ Java/2018/tdd/Main.java
@@ -0,0 +1,18 @@
+package tdd;
+
+/**
+ * This file will be used for beginner test driven development practice.
+ * 
+ * @author Chris F
+ */
+
+/**
+ * Main Program
+ * 
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        
+    }
+}
---
+++ Java/2018/tdd/strings/StringCalculator.java
@@ -0,0 +1,33 @@
+package tdd.strings;
+
+public class stringCalculator {
+
+    /**
+     * Default Constructor
+     */
+    public stringCalculator() {
+
+    }
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    /*
+    public int Add(String num1) {
+        
+        if (num1 == "")
+             return 0;
+        else 
+            return Integer.parseInt(num1);
+    }
+    public int Add(String num1, String num2) {
+
+        return 0;
+    }
+    public int Add(String num1, String num2, String num3) {
+
+        return 0;
+    }
+    */
+}
