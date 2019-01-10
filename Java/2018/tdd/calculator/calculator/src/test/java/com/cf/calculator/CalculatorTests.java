e6df54e670e0060a26a89d02238f07ca14f782e9
cfau214 <c.fauteux@my.ccsu.edu>
2019-01-10 12:02:01 -0500

    Updated JUnit test file.

--- Java/2018/tdd/calculator/calculator/src/test/java/com/cf/calculator/CalculatorTests.java
+++ Java/2018/tdd/calculator/calculator/src/test/java/com/cf/calculator/CalculatorTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.core.IsAnything;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
@@ -15,7 +16,7 @@ public class CalculatorTests {
      * Rigorous Test.
      */
     
-     Calculator calc = new Calculator();     // initialize calculator used in tests.
+    Calculator calc = new Calculator();     // initialize calculator used in tests.
 
     @Test
     public void testApp() {
@@ -26,6 +27,9 @@ public class CalculatorTests {
     public void add_checkThatAddReturnsInteger() {
         int a = 3;
         int b = 7;
-        assertTrue((Integer) calc.add(a,b) instanceof Integer); 
+
+        int result = a+b;
+
+        assertEquals(result, calc.add(3,6));
     }
 }
