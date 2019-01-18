52cba2d3cdd568b1e5d3d5e962a1d88c4a663814
cfau214 <c.fauteux@my.ccsu.edu>
2019-01-18 17:24:13 -0500

    Created Fibonacci sequence generator.

---
+++ Java/2019/numbers/src/home/cf/Fibonacci/FibCalculator.java
@@ -0,0 +1,15 @@
+package home.cf.Fibonacci;
+
+
+/**
+ * FactorChecker is a Singleton that has the appropriate methods to check if an integer has factors.
+ * <p>
+ * This class will maintain a list of factors for the most recent number passed into the getFactors() function.
+ *
+ * @author Chris Fauteux
+ * @since 2019
+ */
+public enum FibCalculator {
+
+
+}
