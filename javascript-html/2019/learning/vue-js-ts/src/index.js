da1e3013f9d00b26ea65829b15c83da999a60993
cfau214 <c.fauteux@my.ccsu.edu>
2019-05-22 21:02:02 -0400

    Set up initial html files and libraries for Vue, JavaScript and Typescript

---
+++ javascript-html/2019/learning/vue-js-ts/src/index.js
@@ -0,0 +1,23 @@
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+let square = (number) => { return number * number }
+
+let chris = new Person("Chris", 29)
+let kino = new Person("Kino", 26)
+
+
+console.log(chris)
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Test Message'
+    }
+})
