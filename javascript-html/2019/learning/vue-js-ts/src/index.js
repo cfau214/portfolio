995476691f85b08ad6ffff3f02f775493aac4fe0
cfau214 <c.fauteux@my.ccsu.edu>
2019-05-22 21:04:23 -0400

    Removed unnecessary code from index.js before beginning Vue project

--- javascript-html/2019/learning/vue-js-ts/src/index.js
+++ javascript-html/2019/learning/vue-js-ts/src/index.js
@@ -1,20 +1,4 @@
 
-class Person {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-    }
-}
-
-let square = (number) => { return number * number }
-
-let chris = new Person("Chris", 29)
-let kino = new Person("Kino", 26)
-
-
-console.log(chris)
-
-
 var app = new Vue({
     el: '#app',
     data: {
