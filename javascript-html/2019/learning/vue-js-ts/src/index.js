86fe20d10c736ce0317765859b8592d83ae205b9
cfau214 <c.fauteux@my.ccsu.edu>
2019-05-24 17:53:28 -0400

    Added span title hovering and styling example using Vue.js

--- javascript-html/2019/learning/vue-js-ts/src/index.js
+++ javascript-html/2019/learning/vue-js-ts/src/index.js
@@ -5,3 +5,26 @@ var app = new Vue({
         message: 'Test Message'
     }
 })
+
+let square = (number) => { return number*number }
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        myMessage: 'From app2: Square of' + square(3)
+    }
+})
+
+var para1 = new Vue({
+    el: '#para1',
+    data: {
+        paraMessage: 'From Paragraph1'
+    }
+})
+
+var title = new Vue({
+    el: '#title1',
+    data: {
+        message: 'Hover over me to see the title!'
+    }
+})
