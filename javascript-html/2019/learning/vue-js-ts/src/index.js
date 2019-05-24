56d3cbea52e7be489481596724bc77da9224a0af
cfau214 <c.fauteux@my.ccsu.edu>
2019-05-24 19:27:53 -0400

    Added transition to a button, v-if conditionals.
    Added Bootstrap to project modules.

--- javascript-html/2019/learning/vue-js-ts/src/index.js
+++ javascript-html/2019/learning/vue-js-ts/src/index.js
@@ -1,12 +1,14 @@
 
+function square (number) { 
+    return number*number 
+}
+
 var app = new Vue({
     el: '#app',
     data: {
         message: 'Test Message'
     }
 })
-
-let square = (number) => { return number*number }
 
 var app2 = new Vue({
     el: '#app2',
@@ -25,6 +27,34 @@ var para1 = new Vue({
 var title = new Vue({
     el: '#title1',
     data: {
-        message: 'Hover over me to see the title!'
+        message: 'Hover over me to see the title!',
+        messageTwo: 'Message two'
+    }
+})
+
+var appTodo = new Vue({
+    el: '#appTodo',
+    data: {
+        todos: [
+            { text: 'Learn Javascript' },
+            { text: 'Learn Vue' },
+            { text: 'Finish M-D AR App' }
+        ]
+    }
+})
+
+let addList = (list) => {
+    let count = 0 
+    for (i = 0; list.length; i++) {
+        count += list[i]
+    }
+    return count > 6
+}
+
+var appIf = new Vue({
+    el: '#appIf',
+    data: {
+        numList: [1,2,3],
+        seen: false
     }
 })
