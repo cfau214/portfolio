494e837de7c2c5122ba973a85f0a3855bebdefb0
cfau214 <c.fauteux@my.ccsu.edu>
2019-06-13 22:08:26 -0400

    Test installing Dart packages via pubmanager.

---
+++ Flutter/2019/flutter-recap/bin/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_recap/flutter_recap.dart' as flutter_recap;
+
+main(List<String> arguments) {
+  print('Hello world: ${flutter_recap.calculate()}!');
+  print('Running from test string: ${flutter_recap.returnString()}');
+}
---
+++ Flutter/2019/flutter-recap/test/flutter_recap_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_recap/flutter_recap.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
