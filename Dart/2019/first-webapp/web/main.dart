7374bdffaae85694e5018e3c0ac45081a0bc5a5a
cfau214 <c.fauteux@my.ccsu.edu>
2019-06-28 17:38:59 -0400

    Started first UI to re-learn CSS.

--- Dart/2019/first-webapp/web/main.dart
+++ Dart/2019/first-webapp/web/main.dart
@@ -4,10 +4,31 @@ void main() {
   querySelector('#output').text = 'Your Dart app is running.';
   querySelector('#testing').text = 'Test Two';
 
-  Element output = querySelector('#LITest');
-  output.children.add(getItem("TEST ONE"));
-  output.children.add(getItem("Test TWO"));
-  output.children.add(getItem("Third Item"));
+  Element uliTest = querySelector('#ULITest');
+  uliTest.children.add(getItem("Test"));
+  uliTest.children.addAll(getList());
+  uliTest.children.add(Element.br());
+  uliTest.children.addAll(getList2(["ListTwoOne", "ListTwoTwo", "ListTwoThree"]));
 }
 
-LIElement getItem(String itemText) => LIElement()..text = itemText;
+UListElement getItem(String itemText) => UListElement()..text = itemText;
+List<UListElement> getList() {
+  var list = <UListElement>[];
+
+  list.add(UListElement()..text = "ItemOne");
+  list.add(UListElement()..text = "ItemTwo");
+  list.add(UListElement()..text = "ItemThree");
+
+  return list;
+}
+
+List<UListElement> getList2(List<String> strList) {
+
+  var ret = <UListElement>[];
+
+  for (String str in strList) {
+    ret.add(UListElement()..text = str);
+  }
+
+  return ret;
+}
