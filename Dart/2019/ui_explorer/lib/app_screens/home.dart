506a639f463d1ebc870a85e771d531cb8f2c57ac
cfau214 <c.fauteux@my.ccsu.edu>
2019-07-13 10:52:42 -0400

    Added alerts, flat/raised buttons.

--- Dart/2019/ui_explorer/lib/app_screens/home.dart
+++ Dart/2019/ui_explorer/lib/app_screens/home.dart
@@ -17,52 +17,100 @@ import 'package:flutter/material.dart';
 //         )
 //       )
 //     );
-//   }  
+//   }
 // }
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar (
-        title: Text('Index List'),
-      ),
-      body: Body()
+    return Scaffold(
+      appBar: AppBar(title: Text('Index List')),
+      body: Body(),
     );
-  }  
+  }
 }
 
 class Body extends StatelessWidget {
-  var _basicStyle = TextStyle(color: Colors.blue, fontSize: 20);
-  var list = <int, String>{
-    1: "Apples",
-    2: "Oranges",
-    3: "Bananas"
-  };
+  final list = <int, String>{1: "Apples", 2: "Oranges", 3: "Bananas"};
 
   @override
   Widget build(BuildContext context) {
-    return Material (      
-      child: Container (
+    return Center(
+      child: Container(
         padding: EdgeInsets.only(left: 10, top: 10),
-        child: Column (
+        child: Column(
           children: <Widget>[
-            for (var index in list.keys) createListRow(index, list[index])
+            for (var index in list.keys) createListRow(index, list[index]),
+            ListButton()
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 
   Row createListRow(int index, String item) {
-    var numberStyle = TextStyle(fontSize: 18, color: Colors.black);
-    var textStyle = TextStyle(fontSize: 18, color: Colors.black);
+    var _style = TextStyle(
+      fontSize: 18,
+      color: Colors.black,
+    );
 
-    return Row (
+    return Row(
       children: <Widget>[
-        Text('$index:  ', style: numberStyle),
-        Expanded(child: Text('$item', style: textStyle))
+        Text('$index:  ', style: _style),
+        Expanded(
+          child: Text('$item', style: _style),
+        )
       ],
+    );
+  }
+}
+
+class ListButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(top: 30.0),
+        width: 250,
+        height: 50,
+        child: RaisedButton(
+          color: Colors.lightBlueAccent,
+          child: Text(
+            "Generate New List",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          elevation: 0,
+          onPressed: () => createAlert(context),
+        ));
+  }
+
+  createAlert(BuildContext context) {
+    var dialog = AlertDialog(
+        title: Text(
+          'New List Generated',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+        ),
+        content: Row(
+          children: <Widget>[
+            Text('Can you see it?', textAlign: TextAlign.center),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                    child: Text('Ok'),
+                    color: Colors.lightBlueAccent,
+                    onPressed: () => Navigator.pop(context)),
+              ],
+            ),
+          ],
+        ));
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => dialog,
     );
   }
 }
