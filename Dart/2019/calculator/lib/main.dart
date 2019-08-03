5a7984774e6444297aabcff59c0e439260c3df41
cfau214 <c.fauteux@my.ccsu.edu>
2019-08-03 07:20:20 -0400

    Added button themes. Commit before changing buttons to button bar.

--- Dart/2019/calculator/lib/main.dart
+++ Dart/2019/calculator/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
 main() => runApp(MaterialApp(
-      title: 'Basic Calculator',
-      home: CalcHome(),
-    ));
+    title: 'Basic Calculator',
+    home: CalcHome(),
+    theme: ThemeData(
+        buttonTheme: ButtonThemeData(
+          colorScheme: ColorScheme.light(
+            primary: Colors.grey[300],
+            
+          ),
+            layoutBehavior: ButtonBarLayoutBehavior.constrained))));
 
 class CalcHome extends StatefulWidget {
   @override
@@ -27,6 +33,8 @@ class _CalcHomeState extends State<CalcHome> {
       body: Container(
         padding: EdgeInsets.all(_minPadding * 10),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             createNumberRow(_numRowOne),
             createNumberRow(_numRowTwo),
@@ -47,26 +55,28 @@ class _CalcHomeState extends State<CalcHome> {
 
     return Row(
       children: list
-          .map((listItem) => 
-                  Expanded(
-                    flex: 1,
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          top: _minPadding,
-                          bottom: _minPadding,
-                          left: _minPadding * 2,
-                          right: _minPadding * 2),
-                      child: Opacity(
-                        opacity: getOpacity(listItem),
-                        child: RaisedButton(
-                          child: Text(listItem.toString()),
-                          onPressed: () {
-                            debugPrint(listItem.toString());
-                          },
-                        ),
-                      ),
-                    )),
-              )
+          .map(
+            (listItem) => Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: _minPadding,
+                    bottom: _minPadding,
+                    left: _minPadding * 2,
+                    right: _minPadding * 2),
+                child: Opacity(
+                  opacity: getOpacity(listItem),
+                  child: RaisedButton(
+                    elevation: 4,
+                    child: Text(listItem.toString()),
+                    onPressed: () {
+                      debugPrint(listItem.toString());
+                    },
+                  ),
+                ),
+              ),
+            ),
+          )
           .toList(),
     );
   }
