a1ea22819e5a8fa4fb4b66344e8b68253d35a26c
cfau214 <c.fauteux@my.ccsu.edu>
2019-08-06 21:04:43 -0400

    Refactored some code. Added '=' operand. Added multiple text fields to display and added color to operand in display.

--- Dart/2019/calc2/lib/main.dart
+++ Dart/2019/calc2/lib/main.dart
@@ -22,15 +22,30 @@ class _Calc2BodyState extends State<Calc2Body> {
   final _buttonRowOne = [7, 8, 9, '/'];
   final _buttonRowTwo = [4, 5, 6, '*'];
   final _buttonRowThree = [1, 2, 3, '-'];
-  final _buttonRowFour = [0, '.', 'C', '+'];
-  final _smallOperandButtons = ['/', '*', '-', '+'];
-  var _lastClicked = "";
+  final _buttonRowFour = [0, '.', 'C', '=', '+'];
+  final _smallOperandButtons = ['/', '*', '-', '+', 'C', '='];
+  var _leftHandSide = "";
+  var _operand = "";
+  var _result = "";
   var _hasDecimal = false;
 
   num getOpacity(dynamic item) => (item is num && item == -1) ? 0.0 : 1.0;
-  num getFlex(dynamic item) => (_smallOperandButtons.contains(item)) ? 2 : 3;
-  Color getColor(dynamic item) =>
-      (item is num) ? Colors.black54 : Colors.red[700];
+  num getFlex(dynamic item) => (_smallOperandButtons.contains(item)) ? 2 : 4;
+  Color getColor(dynamic item) {
+    Color ret;
+
+    if (item == 'C') {
+      ret = Colors.grey[700];
+    } else if (item == '=') {
+      ret = Colors.yellow[500];
+    } else {
+      (item is num || item == '.')
+          ? ret = Colors.black54
+          : ret = Colors.red[700];
+    }
+
+    return ret;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +59,7 @@ class _Calc2BodyState extends State<Calc2Body> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
+            // DISPLAY
             Expanded(
               child: Card(
                 color: Colors.black45,
@@ -53,37 +69,86 @@ class _Calc2BodyState extends State<Calc2Body> {
                     topRight: Radius.elliptical(32, 12),
                   ),
                 ),
-                child: Container(
-                  padding: EdgeInsets.all(50),
-                  alignment: Alignment.topRight,
-                  child: Text(
-                    _lastClicked,
-                    style: TextStyle(
-                      color: Colors.grey[200],
-                      fontSize: 32,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    // OPERAND BOX
+                    Container(
+                      padding: EdgeInsets.only(right: 50),
+                      alignment: Alignment.center,
+                      child: Text(
+                        _operand,
+                        style: TextStyle(
+                          color: Colors.yellow[200],
+                          fontSize: 32,
+                        ),
+                      ),
                     ),
-                  ),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(right: 50, top: 20),
+                          alignment: Alignment.topRight,
+                          child: Text(
+                            _leftHandSide,
+                            style: TextStyle(
+                              color: Colors.grey[200],
+                              fontSize: 32,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 50, top: 20),
+                          alignment: Alignment.topRight,
+                          child: Text(
+                            _leftHandSide,
+                            style: TextStyle(
+                              color: Colors.grey[200],
+                              fontSize: 32,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 50, top: 20),
+                          alignment: Alignment.topRight,
+                          child: Text(
+                            _leftHandSide,
+                            style: TextStyle(
+                              color: Colors.grey[200],
+                              fontSize: 32,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
+
+            // BUTTONS
             Row(
-              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
+                // 7-9
                 ...buildButtonRow(_buttonRowOne),
               ],
             ),
             Row(
               children: <Widget>[
+                // 4-6
                 ...buildButtonRow(_buttonRowTwo),
               ],
             ),
             Row(
               children: <Widget>[
+                // 1-3
                 ...buildButtonRow(_buttonRowThree),
               ],
             ),
             Row(
               children: <Widget>[
+                // 0
                 ...buildButtonRow(_buttonRowFour),
               ],
             ),
@@ -94,10 +159,46 @@ class _Calc2BodyState extends State<Calc2Body> {
   }
 
   List<Widget> buildButtonRow(List<dynamic> list) {
-
-    reset () {
-      _lastClicked = "";
+    reset() {
+      _leftHandSide = "";
+      _operand = "";
+      _result = "";
       _hasDecimal = false;
+    }
+
+    setOperand(String op) {
+      _operand = op;
+    }
+
+    getResult() {}
+
+    processButtonAction(dynamic item) {
+      if (item is String) {
+        switch (item) {
+          case 'C':
+            reset();
+            break;
+
+          case '.':
+            if (!_hasDecimal) {
+              _leftHandSide += item;
+              _hasDecimal = true;
+            }
+            break;
+
+          case '=':
+            getResult();
+            break;
+
+          default:
+            setOperand(item);
+            break;
+        }
+      }
+      // Item is num
+      else {
+        _leftHandSide += item.toString();
+      }
     }
 
     return list
@@ -119,17 +220,7 @@ class _Calc2BodyState extends State<Calc2Body> {
                     onPressed: () {
                       HapticFeedback.vibrate();
                       setState(() {
-                        if (item == 'C') {
-                          reset();
-                        } else if (item == '.') {  
-                          
-                          if (!_hasDecimal) {
-                            _lastClicked += item;
-                            _hasDecimal = true;
-                          }
-                        } else {
-                          _lastClicked += item.toString();
-                        }
+                        processButtonAction(item);
                       });
                     },
                     shape: Border.all(
@@ -142,5 +233,3 @@ class _Calc2BodyState extends State<Calc2Body> {
         .toList();
   }
 }
-
-// color: Colors.black54,
