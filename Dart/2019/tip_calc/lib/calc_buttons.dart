7a6f8db93d68b2a8b5afd627f45926265729a0a4
cfau214 <c.fauteux@my.ccsu.edu>
2019-08-10 17:41:06 -0400

    Moved Button-related globals to their own class. Buttons enumerator and the map used for each button label were consolidated in this class.

--- Dart/2019/tip_calc/lib/calc_buttons.dart
+++ Dart/2019/tip_calc/lib/calc_buttons.dart
@@ -4,43 +4,7 @@ import 'package:provider/provider.dart';
 
 import 'results.dart';
 import 'amount_provider.dart';
-
-/// Buttons
-///
-/// Enumerator used for processing each button's action and label through a [buttonMap]
-/// Used primarily in conjunction with each button action in [ButtonRow].
-enum Buttons {
-  one,
-  two,
-  three,
-  four,
-  five,
-  six,
-  seven,
-  eight,
-  nine,
-  zero,
-  delete,
-  calculate
-}
-
-/// buttonMap
-///
-/// This map allows the label for a button to change based on it's enumerated [Buttons] type.
-var buttonMap = <Buttons, dynamic>{
-  Buttons.one: "1",
-  Buttons.two: "2",
-  Buttons.three: "3",
-  Buttons.four: "4",
-  Buttons.five: "5",
-  Buttons.six: "6",
-  Buttons.seven: "7",
-  Buttons.eight: "8",
-  Buttons.nine: "9",
-  Buttons.zero: "0",
-  Buttons.delete: Icon(Icons.backspace, size: 30),
-  Buttons.calculate: Icon(Icons.arrow_right, size: 60)
-};
+import 'buttons.dart';
 
 /// CalcButtons creates the layout for the calculator.
 ///
