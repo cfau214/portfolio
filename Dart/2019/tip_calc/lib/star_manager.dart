abd4c0e1b64da9921d324e064f7e022f8650c941
cfau214 <c.fauteux@my.ccsu.edu>
2019-08-13 20:38:33 -0400

    Added suggestion file, TODO

--- Dart/2019/tip_calc/lib/star_manager.dart
+++ Dart/2019/tip_calc/lib/star_manager.dart
@@ -2,17 +2,17 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 /// StarManager is used to track the state of each [Star] in the star rating system.
-/// 
-/// On first initialization, a default of 5 Stars are created and added to the [starList]. 
-/// As a specific star is toggled, the manager will toggle that star and all of it's preceding stars 
-/// similarly, 
-/// 
+///
+/// On first initialization, a default of 5 Stars are created and added to the [starList].
+/// As a specific star is toggled, the manager will toggle that star and all of it's preceding stars
+/// similarly,
+///
 ///   eg. When the user clicks the 3rd star in the list, the first and second stars will be toggled
 ///   to match the status of the 3rd star.
-/// 
+///
 /// * [starRating] - Returns the number of stars currently selected by the user.
 /// * [starList] - List of stars being tracked by the manager.
-/// 
+///
 class StarManager with ChangeNotifier {
   StarManager() {
     _init();
@@ -30,10 +30,22 @@ class StarManager with ChangeNotifier {
   toggleStar(Star tempStar) {
     var index = tempStar.index;
 
-    starList[index].toggle();
+    // If the user selects a star that precedes the currently selected stars,
+    // only change the status of stars that come AFTER the new star.
+    if (index < starRating - 1) {
+      for (int i = index + 1; i < starList.length; i++) {
+        starList[i].isSelected = false;
+      }
+    } 
+    // Star rating must be at least 1.
+    else if (index == 0) {
+    } else {
+      starList[index].toggle();
 
-    for (int i = 0; i < index; i++) {
-      starList[i].isSelected = starList[index].isSelected;
+      // Change the status of all stars preceding tempStar to match tempStar's new status.
+      for (int i = 0; i < index; i++) {
+        starList[i].isSelected = starList[index].isSelected;
+      }
     }
 
     notifyListeners();
@@ -41,16 +53,21 @@ class StarManager with ChangeNotifier {
 }
 
 /// Star is used in the [StarManager] as a way to track user's service satisfaction.
-/// 
+///
 /// The star can return an [icon] representing the current status that shows a different
 /// icon depending on if the star [isSelected].
-/// 
+///
 class Star {
-  Star(this.index);
+  Star(this.index) {
+    // TODO: Should we default the first star to be selected or should we allow 0 rating?
+    if (index == 0) {
+      this.isSelected = true;
+    }
+  }
 
   int index;
   bool isSelected = false;
-  Color iconColor = Colors.yellow;
+  Color iconColor = Colors.yellow[600];
 
   Icon get icon => Icon(
         isSelected ? Icons.star : Icons.star_border,
