874a841baad94eee84bcdb688536f6d90620d498
cfau214 <c.fauteux@my.ccsu.edu>
2019-08-13 20:14:39 -0400

    Added comments and documentation to star_manager class.

--- Dart/2019/tip_calc/lib/star_manager.dart
+++ Dart/2019/tip_calc/lib/star_manager.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
+/// StarManager is used to track the state of each [Star] in the star rating system.
+/// 
+/// On first initialization, a default of 5 Stars are created and added to the [starList]. 
+/// As a specific star is toggled, the manager will toggle that star and all of it's preceding stars 
+/// similarly, 
+/// 
+///   eg. When the user clicks the 3rd star in the list, the first and second stars will be toggled
+///   to match the status of the 3rd star.
+/// 
+/// * [starRating] - Returns the number of stars currently selected by the user.
+/// * [starList] - List of stars being tracked by the manager.
+/// 
 class StarManager with ChangeNotifier {
   StarManager() {
     _init();
@@ -28,6 +40,11 @@ class StarManager with ChangeNotifier {
   }
 }
 
+/// Star is used in the [StarManager] as a way to track user's service satisfaction.
+/// 
+/// The star can return an [icon] representing the current status that shows a different
+/// icon depending on if the star [isSelected].
+/// 
 class Star {
   Star(this.index);
 
