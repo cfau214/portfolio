085d75883915f8f0b03fe10757817966bbfb6bfc
cfau214 <c.fauteux@my.ccsu.edu>
2019-09-06 19:02:54 -0400

    Added default theme for project.

--- Dart/2019/app_manager/lib/main.dart
+++ Dart/2019/app_manager/lib/main.dart
@@ -5,11 +5,18 @@ import 'apps/tip_calc/tip_calculator.dart';
 
 main() => runApp(MyApp());
 
+final ThemeData _themeData = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.lightBlue,
+  accentColor: Colors.lightBlueAccent,
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App Manager',
+      theme: _themeData,
       home: ManagerSplash(),
       routes: <String, WidgetBuilder> {
         '/home' : (BuildContext context) => ManagerHome(),
