a09ed9f2d504d63f0dc438ab6dfd0d87f9ad1f5f
cfau214 <c.fauteux@my.ccsu.edu>
2019-09-08 18:30:17 -0400

    Refactored themes into separate file to prepare for theme change switch.

--- Dart/2019/app_manager/lib/main.dart
+++ Dart/2019/app_manager/lib/main.dart
@@ -1,27 +1,17 @@
 import 'package:app_manager/navigator.dart';
 import 'package:flutter/material.dart';
+
 import 'screens/manager_splash.dart';
+import 'themes.dart' as themes;
 
 main() => runApp(MyApp());
-
-final ThemeData _themeData = ThemeData(
-  brightness: Brightness.light,
-  primarySwatch: Colors.lightBlue,
-  accentColor: Colors.lightBlueAccent,
-  primaryTextTheme: Typography.whiteCupertino,
-  textTheme: Typography.whiteCupertino,
-  buttonTheme: ButtonThemeData(
-    buttonColor: Colors.lightBlue,
-    textTheme: ButtonTextTheme.normal,
-  ),
-);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App Manager',
-      theme: _themeData,
+      theme: themes.light,
       home: ManagerSplash(),
       routes: MyNavigator.routes,
     );
---
+++ Dart/2019/app_manager/lib/themes.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+final ThemeData light = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.lightBlue,
+  accentColor: Colors.lightBlueAccent,
+  primaryTextTheme: Typography.whiteCupertino,
+  textTheme: Typography.whiteCupertino,
+  buttonTheme: ButtonThemeData(
+    buttonColor: Colors.lightBlue,
+    textTheme: ButtonTextTheme.normal,
+  ),
+);
+
+final ThemeData dark = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.lightBlue,
+  accentColor: Colors.lightBlueAccent,
+  primaryTextTheme: Typography.whiteCupertino,
+  textTheme: Typography.whiteCupertino,
+  buttonTheme: ButtonThemeData(
+    buttonColor: Colors.lightBlue,
+    textTheme: ButtonTextTheme.normal,
+  ),
+);
