4b160c2365126e5609f12a3497e255ee1c0049be
cfau214 <c.fauteux@my.ccsu.edu>
2019-09-08 08:22:32 -0400

    Added Grid View to App Manager. Added Tip Calculator app. Added Drawer to Manager Home.

--- Dart/2019/app_manager/lib/screens/manager_home.dart
+++ Dart/2019/app_manager/lib/screens/manager_home.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:app_manager/shared_widgets/app_bar.dart';
 
-class ManagerHome extends StatelessWidget{
+//TODO:
+// [x] - Update Tip calculator to use new appBar
+// [x] - Fix overflow issues on tip calc.
+// [x] - Add Switch widget for dynamic theme
+// [x] - Custom appBar with Drawer that can be added to all new apps.
+
+class ManagerHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextStyle _drawerStyle = TextStyle(color: Colors.black);
+
     return Scaffold(
       appBar: namedAppBar('App Manager'),
+      body: ManagerHomeBody(),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Test Header', style: _drawerStyle),
+            ),
+            ListTile(title: Text('Test Title', style: _drawerStyle)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ManagerHomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      padding: EdgeInsets.all(20),
+      children: <Widget>[
+        GridTile(
+          child: IconButton(
+            tooltip: 'Tip Calculator',
+            icon: Icon(
+              Icons.payment,
+              color: Colors.lightGreen,
+            ),
+            iconSize: 70,
+            onPressed: () {
+              Navigator.pushNamed(context, '/tipCalculator');
+            },
+          ),
+        ),
+      ],
     );
   }
 }
--- Dart/2019/app_manager/lib/shared_widgets/app_bar.dart
+++ Dart/2019/app_manager/lib/shared_widgets/app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 Widget namedAppBar(String title) {
   return AppBar(
+    centerTitle: true,
     title: Text(title),
   );
 }
